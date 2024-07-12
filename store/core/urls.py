50008346c22ee3d9431d55efb95493e503375bda
x86skwizer <yassineamrire@protonmail.com>
2024-07-12 03:18:59 +0100

    Create BaseHTML TopBar/Footer

--- store/core/urls.py
+++ store/core/urls.py
@@ -4,6 +4,9 @@ from . import views
 app_name = "core"
 
 urlpatterns = [
+	path('index.html', views.index, name='index'),
 	path('', views.index, name='index'),
 	path('contact.html', views.contact, name='contact'),
+	path('contact/', views.contact, name='contact'),
+	path('contact', views.contact, name='contact'),
 ]
