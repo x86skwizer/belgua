dadba6efb6e7634b82db5e60e7e9a67d3ba1f072
x86skwizer <yassineamrire@protonmail.com>
2024-07-12 06:34:47 +0100

    Fix URL Tag

--- store/core/urls.py
+++ store/core/urls.py
@@ -1,33 +1,16 @@
 from django.urls import path
 from . import views
 
-app_name = "core"
+app_name = 'core'
 
 urlpatterns = [
-	path('index.html', views.index, name='index'),
 	path('', views.index, name='index'),
-	path('contact.html', views.contact, name='contact'),
 	path('contact/', views.contact, name='contact'),
-	path('contact', views.contact, name='contact'),
-	path('cart.html', views.cart, name='cart'),
 	path('cart/', views.cart, name='cart'),
-	path('cart', views.cart, name='cart'),
-	path('product-list.html', views.product_list, name='product-list'),
 	path('product-list/', views.product_list, name='product-list'),
-	path('product-list', views.product_list, name='product-list'),
-	path('product-detail.html', views.product_detail, name='product-detail'),
 	path('product-detail/', views.product_detail, name='product-detail'),
-	path('product-detail', views.product_detail, name='product-detail'),
-	path('checkout.html', views.checkout, name='checkout'),
 	path('checkout/', views.checkout, name='checkout'),
-	path('checkout', views.checkout, name='checkout'),
-	path('my-account.html', views.my_account, name='my-account'),
 	path('my-account/', views.my_account, name='my-account'),
-	path('my-account', views.my_account, name='my-account'),
-	path('wishlist.html', views.wishlist, name='wishlist'),
 	path('wishlist/', views.wishlist, name='wishlist'),
-	path('wishlist', views.wishlist, name='wishlist'),
-	path('login.html', views.login, name='login'),
 	path('login/', views.login, name='login'),
-	path('login', views.login, name='login'),
 ]
