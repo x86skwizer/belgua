7c39a33f622aae376fc874807db16246a092c2c5
x86skwizer <yassineamrire@protonmail.com>
2024-07-16 13:13:29 +0100

    Add product to cart with jquery

--- store/cart/cart.py
+++ store/cart/cart.py
@@ -11,3 +11,11 @@ class Cart():
 
 		# Make sure cart is available on all site
 		self.cart = cart
+
+	def add(self, product):
+		product_id = str(product.id)
+		if product_id in self.cart:
+			pass
+		else:
+			self.cart[product_id] = {'price': str(product.price)}
+		self.session.modified = True
--- store/cart/views.py
+++ store/cart/views.py
@@ -1,11 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .cart import Cart
+from core.models import Product
+from django.http import JsonResponse
 
 # Create your views here.
 def cart_summary(request):
 	return render(request, "cart_summary.html", {})
 
 def cart_add(request):
-	pass
+    cart = Cart(request)
+    
+    if request.method == 'POST' and request.POST.get('action') == 'post':
+        product_id = request.POST.get('product_id')
+        
+        print("Received POST data:", request.POST)  # Debugging line
+
+        if product_id:
+            try:
+                product_id = int(product_id)
+                product = get_object_or_404(Product, id=product_id)
+                cart.add(product=product)
+                return JsonResponse({'message': 'Product added to cart successfully.', 'product_name': product.name})
+            
+            except (ValueError, Product.DoesNotExist):
+                return JsonResponse({'error': 'Invalid product ID or product does not exist.'}, status=400)
+        else:
+            return JsonResponse({'error': 'Product ID is missing.'}, status=400)
+    
+    return JsonResponse({'error': 'Invalid request method or action.'}, status=405)
 
 def cart_delete(request):
 	pass
