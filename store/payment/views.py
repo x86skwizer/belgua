5095a6f6178d39c6c067e3752e56b2d48c30022d
x86skwizer <yassineamrire@protonmail.com>
2024-07-18 22:18:03 +0100

    Billing Info

--- store/payment/views.py
+++ store/payment/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
-from payment.forms import ShippingForm
+from django.shortcuts import render, redirect
+from payment.forms import ShippingForm, PaymentForm
 from payment.models import ShippingAddress
 from cart.cart import Cart
+from django.contrib import messages
+
 
 # Create your views here.
 def checkout(request):
@@ -21,9 +23,37 @@ def checkout(request):
 	else:
 		# Checkout as guest
 		shipping_form = ShippingForm(request.POST or None)
-		return render(request, "payment/checkout.html", {"cart_products":cart_products, "quantities":quantities, "totals":totals, "shipping_form":shipping_form})
+		return render(request, "payment/checkout.html", {"cart_products":cart_products, "quantities":quantities, "totals":totals})
 
-	
+def billing_info(request):
+	if request.POST:
+		# Get the cart
+		cart = Cart(request)
+		cart_products = cart.get_prods
+		quantities = cart.get_quants
+		totals = cart.cart_total()
+
+	# Check to see if user is logged in
+		if request.user.is_authenticated:
+			# Get The Billing Form
+			billing_form = PaymentForm()
+			return render(request, "payment/billing_info.html", {"cart_products":cart_products, "quantities":quantities, "totals":totals, "shipping_info":request.POST, "billing_form":billing_form})	
+
+			
+
+		else:
+			# Not logged in
+			# Get The Billing Form
+			billing_form = PaymentForm()
+			return render(request, "payment/billing_info.html", {"cart_products":cart_products, "quantities":quantities, "totals":totals, "shipping_info":request.POST, "billing_form":billing_form})
+
+
+		
+		shipping_form = request.POST
+		return render(request, "payment/billing_info.html", {"cart_products":cart_products, "quantities":quantities, "totals":totals, "shipping_form":shipping_form})
+	else:
+		messages.success(request, "Access Denied")
+		return redirect('core:index')
 
 def payment_success(request):
 	return render(request, "payment/payment_success.html", {})
