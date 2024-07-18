3b0cce642488da02d2149a87bfb40919c7d95771
x86skwizer <yassineamrire@protonmail.com>
2024-07-18 23:23:55 +0100

    Attach Order Items

--- store/payment/views.py
+++ store/payment/views.py
@@ -4,6 +4,7 @@ from payment.models import ShippingAddress, Order, OrderItem
 from django.contrib.auth.models import User
 from cart.cart import Cart
 from django.contrib import messages
+from core.models import Product
 
 
 # Create your views here.
@@ -33,6 +34,23 @@ def process_order(request):
 			# Create Order
 			create_order = Order(user=user, full_name=full_name, email=email, shipping_address=shipping_address, amount_paid=amount_paid)
 			create_order.save()
+
+			# Add order items
+
+			# Get the order ID
+			order_id = create_order.pk
+			# Get product Info
+			for product in cart_products():
+				# Get product ID
+				product_id = product.id
+				# Get product price
+				price = product.price
+				# Get quantity
+				for key,value in quantities().items():
+					if int(key) == product.id:
+						# Create order item
+						create_order_item = OrderItem(order_id=order_id, product_id=product_id, user=user, quantity=value, price=price)
+						create_order_item.save()
 			messages.success(request, "Order Placed !")
 			return redirect('core:index')
 		else:
@@ -40,6 +58,27 @@ def process_order(request):
 			# Create Order
 			create_order = Order(full_name=full_name, email=email, shipping_address=shipping_address, amount_paid=amount_paid)
 			create_order.save()
+
+			# Add order items
+			
+			# Get the order ID
+			order_id = create_order.pk
+			
+			# Get product Info
+			for product in cart_products():
+				# Get product ID
+				product_id = product.id
+				# Get product price
+				if product.is_sale:
+					price = product.sale_price
+				else:
+					price = product.price
+				# Get quantity
+				for key,value in quantities().items():
+					if int(key) == product.id:
+						# Create order item
+						create_order_item = OrderItem(order_id=order_id, product_id=product_id, quantity=value, price=price)
+						create_order_item.save()
 			messages.success(request, "Order Placed !")
 			return redirect('core:index')
 	else:
