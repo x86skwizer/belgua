82d30b864408631f72d5777329d606de49c236e7
x86skwizer <yassineamrire@protonmail.com>
2024-07-18 16:53:51 +0100

    Payment and shipping

--- store/store/settings.py
+++ store/store/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     # Custom Apps
 	'core',
 	'cart',
+	'payment'
 ]
 
 MIDDLEWARE = [
--- store/store/urls.py
+++ store/store/urls.py
@@ -23,5 +23,6 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
 	path('', include('core.urls')),
-	path('cart/', include('cart.urls'))
+	path('cart/', include('cart.urls')),
+	path('payment/', include('payment.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
